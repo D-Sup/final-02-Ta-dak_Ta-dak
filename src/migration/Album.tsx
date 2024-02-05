d94f396bf10d6c860ecccc1437235bc1a2df052d
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-02-05 22:09:11 +0900

    [ts-migrate][migration] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- src/migration/Album.tsx
+++ src/migration/Album.tsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+// @ts-expect-error TS(7016): Could not find a declaration file for module 'styl... Remove this comment to see the full error message
 import styled from 'styled-components';
 
-export default function Album({post}) {
+export default function Album({
+  post
+}: any) {
   return (
     post.image && 
+    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <AlbumStyle>
+      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Link to={`/postdetail/${post.id}`}>
+        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <img
           src={post.image}
           alt={`${post.author.username}의 포스팅 이미지`}
